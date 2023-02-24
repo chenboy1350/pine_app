198604f807249b83df208c4b2d08c7baafb3f6da
chenboy1350 <chain456_@hotmail.com>
2023-02-24 21:41:41 +0700

    compare result

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Pine_Tree
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello Pine!");    
+        // int j = 9, k, l;
+        // for (int i = 1; i <= j; i++)
+        // {  
+        //     for (k = 1; k <= (j - i); k++) 
+        //         Console.Write(" ");  
+        //     for (l = 1; l <= i; l++)
+        //         Console.Write(i);  
+        //     for (l = (i - 1); l >= 1; l--)
+        //         Console.Write(i);  
+        //     Console.WriteLine();  
+        // } 
+
+        Console.WriteLine("1");
+        Console.WriteLine("22");  
+        Console.WriteLine("333");  
+        Console.WriteLine("4444");  
+        Console.WriteLine("55555");  
+        Console.WriteLine("666666");  
+        Console.WriteLine("7777777");  
+        Console.WriteLine("88888888"); 
+        Console.WriteLine("999999999");  
     }
   }
 }
